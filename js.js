9f5fab52d53ec372426f773d605542d0c4893d29
Zaid Fadel <zaid.fadel89@gmail.com>
2019-09-13 22:02:22 -0500

    complete

---
+++ js.js
@@ -0,0 +1,129 @@
+// hi i deside not to get anything from google or youtube
+$(document).ready(function() {
+  var options = [
+    {
+      question:
+        'Pupusas, handmade thick stuffed corn tortillas, are a traditional dish from what country?',
+      choice: ['Ethiopia', 'El Salvadore', 'Peru', 'Guatamala'],
+      answer: 'El Salvadore',
+      photo: 'assets/images/pupusas.jpg',
+      name: 'q01'
+    },
+    {
+      question: 'Kopi luwak is a very expensive type of what?',
+      choice: ['Spice', 'Caviar', 'Coffee', 'Rice variety'],
+      answer: 'Coffee',
+      photo: 'assets/images/coffee.gif',
+      name: 'q02'
+    },
+    {
+      question:
+        'What popular soda beverage was originally developed as a mixer for whiskey?',
+      choice: ['Mountain Dew', 'Sprite', '7-UP', 'Coke'],
+      answer: 'Mountain Dew',
+      photo: 'assets/images/mtdew.gif',
+      name: 'q03'
+    }
+  ];
+  var correctCount = 1;
+  var wrongCount = 0;
+  var unanswerCount = 0;
+  var timer = 20;
+  var intervalId;
+  var userGuess = '';
+  var running = false;
+  var qCount = options.length;
+  var pick;
+  var index = 0;
+  var newArray = [];
+  //maybe an extra step you dont need
+  var holder = [];
+  $('#submit').hide();
+
+  $('#reset').hide();
+  // now we start game to start the game
+  $('#start').on('click', function() {
+    $('#start').hide();
+    $('#submit').show();
+    $('#correctCount').show;
+
+    $(document).on('click', '#submit', function() {
+      // alert($(`input[name=${pick.name}]:checked`).val());
+      $('#timers').text(runTimer);
+      var userSelection = $(`input[name=${pick.name}]:checked`).val();
+      console.log('userSelection' + userSelection);
+      // console.log(pick);
+      if (userSelection == pick.answer) {
+        alert('correct');
+        alert('you have ' + correctCount + ' Correct answer ');
+        correctCount++;
+
+        index++;
+        displayQuestion();
+        console.log('correct: ' + correctCount);
+      } else {
+        wrongCount++;
+        alert('Incorrect');
+        alert('you have ' + wrongCount + ' uncorrect answer ');
+        index++;
+        displayQuestion();
+      }
+    });
+    runTimer();
+    for (var i = 0; i < options.length; i++) {}
+    displayQuestion();
+  });
+
+  // timer start
+  function runTimer() {
+    if (!running) {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+      running = true;
+    }
+  }
+
+  // time countdown
+  function decrement() {
+    $('#timeleft').html('Time remaining ' + timer);
+    timer--;
+    if (timer === 0) {
+      correctCount++;
+
+      clearInterval(intervalId);
+      wrongCount++;
+      stop('');
+      $('#answerblock').html(
+        '<p>Time is up! The correct answer is: ' + pick.choice[1] + '</p>'
+      );
+    }
+  }
+  //randomly pick question in array
+  function displayQuestion() {
+    //generate random index in array
+    index = Math.floor(Math.random() * options.length);
+    pick = options[index];
+    // pick = options[0];
+    console.log(pick);
+
+    var newQuestion = $('<form>');
+
+    $('#answerblock').append(newQuestion);
+
+    $('#questionblock').html('<h2>' + pick.question + '</h2>');
+    for (var i = 0; i < pick.choice.length; i++) {
+      userGuess = $('<div>');
+      newQuestion.append(
+        `<input type="radio" name="${pick.name}" value="${pick.choice[i]}">${pick.choice[i]}`
+      );
+
+      // userChoice.addClass('answerchoice');
+      // userChoice.html(pick.choice[i]);
+      //assign array position to it so can check answer
+      // userChoice.attr('data-guessvalue', pick.choice[i]);
+      // $(newQuestion).append(userChoice);
+    }
+
+    $('#answerblock').append(newQuestion);
+  }
+});
